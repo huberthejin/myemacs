90a18dc42be7e009b30cae498f779580f03010fc
hjin <hjin@exmaple.com>
2019-12-16 21:09:19 -0500

    cleanup

--- custom/xcscope-setup.el
+++ custom/xcscope-setup.el
@@ -7,9 +7,9 @@
     (define-key global-map [(ctrl f4)] 'cscope-history-backward-line-current-result)
 
     ;; cscope-setup is not working for whatever reason, so I just add the ones that I need.
-    (global-set-key (kbd "C-c s s") 'cscope-find-this-symbol)
-    (global-set-key (kbd "C-c s t") 'cscope-find-this-text-string)
-    (global-set-key (kbd "C-c s e") 'cscope-find-egrep-pattern)
+    ;;(global-set-key (kbd "C-c s s") 'cscope-find-this-symbol)
+    ;;(global-set-key (kbd "C-c s t") 'cscope-find-this-text-string)
+    ;;(global-set-key (kbd "C-c s e") 'cscope-find-egrep-pattern)
     )
   )
 
