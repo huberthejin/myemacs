58aa66eb0d3f49433aca54c37dd5c36ef8f96ea1
hjin <hjin@infinera.com>
2020-01-22 20:43:17 +0000

    kill-other-buffers

--- custom/general-setup.el
+++ custom/general-setup.el
@@ -334,6 +334,22 @@
   (interactive)
   (revert-buffer :ignore-auto :noconfirm))
 
+(defun kill-all-dired-buffers ()
+  "Kill all dired buffers."
+  (interactive)
+  (save-excursion
+    (let ((count 0))
+      (dolist (buffer (buffer-list))
+        (set-buffer buffer)
+        (when (equal major-mode 'dired-mode)
+          (setq count (1+ count))
+          (kill-buffer buffer)))
+      (message "Killed %i dired buffer(s)." count))))
 
+;; Kill all other buffers. Only keep the current one.
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
 
 (provide 'general-setup)
