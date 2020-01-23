48e7b5884f06858243dfdb81175715d9a6450231
hjin <hjin@exmaple.com>
2020-01-22 19:20:34 -0500

    close all buffers

--- custom/general-setup.el
+++ custom/general-setup.el
@@ -334,22 +334,15 @@
   (interactive)
   (revert-buffer :ignore-auto :noconfirm))
 
-(defun kill-all-dired-buffers ()
-  "Kill all dired buffers."
-  (interactive)
-  (save-excursion
-    (let ((count 0))
-      (dolist (buffer (buffer-list))
-        (set-buffer buffer)
-        (when (equal major-mode 'dired-mode)
-          (setq count (1+ count))
-          (kill-buffer buffer)))
-      (message "Killed %i dired buffer(s)." count))))
-
-;; Kill all other buffers. Only keep the current one.
+;; Close all other buffers. Only keep the current one.
 (defun kill-other-buffers ()
   "Kill all other buffers."
   (interactive)
   (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+
+;; Close all buffers.
+(defun kill-all-buffers ()
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
 
 (provide 'general-setup)
