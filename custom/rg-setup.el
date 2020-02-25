95e30247eda23d917e7aafa03e0126599b35297a
He Jin <hubjin@cisco.com>
2020-02-24 21:35:56 -0500

    Add cc and ch

--- custom/rg-setup.el
+++ custom/rg-setup.el
@@ -4,6 +4,9 @@
   (progn
     (setq rg-keymap-prefix (kbd "C-c r"))
     (rg-enable-default-bindings)
+    (setq rg-custom-type-aliases
+  '(("cc" .    "*.[chH]  *.[chH].in  *.cats *.enum")
+    ("ch" .    "*.[hH]  *.[hH].in  *.cats *.enum")))
     )
   )
 
