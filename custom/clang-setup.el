432736025477ab708b816730d5a4f8dba91b7e90
He Jin <hubjin@cisco.com>
2020-04-16 21:07:26 -0400

    turn on/off clang-format on the fly

--- custom/clang-setup.el
+++ custom/clang-setup.el
@@ -4,6 +4,7 @@
   (progn
     (global-set-key (kbd "C-c f r") 'clang-format-region)
     (global-set-key (kbd "C-c f b") 'clang-format-buffer)
+    (global-set-key (kbd "C-c f f") 'clang-format+-mode)
     ;;(setq clang-format-style "file")
     ;;(setq clang-format-style "{BasedOnStyle: llvm, IndentWidth: 4, AccessModifierOffset: -4}")
     )
@@ -11,11 +12,11 @@
 
 ;; clang+
 ;; Used for format during save disabled for now
-;;(use-package clang-format+
-;;  :config
-;;  (progn
-;;    (add-hook 'c-mode-common-hook #'clang-format+-mode))
-;;    (setq clang-format+-context 'modification)
-;;  )
+(use-package clang-format+
+  :config
+  (progn
+    (add-hook 'c-mode-common-hook #'clang-format+-mode))
+    (setq clang-format+-context 'modification)
+  )
 
 (provide 'clang-setup)
