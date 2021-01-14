f4ab60bd2a0e719bfeb504fc849c2455a57c7b31
Hubert Jin <hubjin@cisco.com>
2021-01-13 19:53:07 -0500

    add more file types

--- custom/rg-setup.el
+++ custom/rg-setup.el
@@ -5,7 +5,7 @@
     (setq rg-keymap-prefix (kbd "C-c r"))
     (rg-enable-default-bindings)
     (setq rg-custom-type-aliases
-  '(("cc" .    "*.[chH]  *.[chH].in *.in *.cats *.enum *.enumh *.enumc")
+  '(("cc" .    "*.[chH]  *.[chH].in *.in *.cc *.cats *.enum *.enumh *.enumc *.pl *.bag *.yang")
     ("ch" .    "*.[hH]  *.[hH].in  *.cats *.enum *.enumh *.enumc")
     ("cmd" .    "*.cmd")
     ))
